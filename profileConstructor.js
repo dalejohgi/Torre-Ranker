941140ca48d061fa17470d6ce0dec01b624f25de
David <1508@holbertonschool.com>
2020-11-14 01:22:54 -0500

    Create class constructor for users

--- profileConstructor.js
+++ profileConstructor.js
@@ -5,26 +5,30 @@
 
 const fetch = require('node-fetch');
 
-
-// Class constructor for Jolby's objects
-exports.User = class User { constructor(name, picture, company, time, image, applyLink, tags = [], reported=false) {
-  this.title = title;
-  this.id = id;
-  this.company = company;
-  this.time = "";
-  this.image = image;
-  this.applyLink = applyLink;
-  this.tags = tags;
-  this.reported = reported;
+// User constructor
+class User { constructor(name, picture, jobs, projects) {
+  this.name = name;
+  this.picture = picture;
+  this.jobs = jobs;
+  this.projects = projects;
   }
 };
 
-// Returns the descrption of an offer pased its url and a Jquery string
-exports.getDesc = async function (url, queryString) {
-  const res = await fetch(url, {
-    headers: { 'User-Agent': 'Chrome/51.0.2704.103' }
-  });
-  const html2 = await res.text();
-  const $$ = cheerio.load(html2);
-  return $$(queryString).text();
+// Consumes Torre API to get an user object
+exports.getUser = async function (username="") {
+  if (username) {
+    let response = await fetch('https://bio.torre.co/api/bios/' + username);
+    const userObj = await response.json();
+
+    const name = userObj.person.name;
+    const picture = userObj.person.picture;
+    const jobs = userObj.person.jobs;
+    const projects = userObj.person.jobs;
+    const user = new User(name, picture, jobs, projects);
+    return JSON.stringify(user);
+  }
 };
+
+
+
+
